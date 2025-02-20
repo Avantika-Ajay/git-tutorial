9639c533158442fae28c635855d2524336baaf01
Avantika Ajay Poduval <avantikapoduval@gmail.com>
2025-02-20 09:38:54 +0530

    version 1 restored

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('index.js');
-console.log('version3updated');
+console.log('version 1');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('config.js');
-console.log('version3updated');
+console.log('version 1');
